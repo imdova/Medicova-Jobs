8a0a33da139767b8cf88d845c09921a84a83414d
AbdElRahman501 <abdelrahman.6182@gmail.com>
2024-12-29 16:03:17 +0200

    rout pages coloring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,9 @@ import Header from "./(public)/search/header";
 import Image from "next/image";
 import Link from "next/link";
 import SearchForm from "@/components/UI/search-form";
-import { ArrowBack, ArrowForward, Person } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
 import Flag from "@/components/UI/flagitem";
 import Carousel, { CompanyCard, SpecialtyCard } from "@/components/UI/Carousel";
 import { companies, specialists } from "@/constants";
-import { Ellipse6, IdeaIcon } from "@/components/icons/icons";
 import BasicTabs from "@/components/UI/tabs";
 import Testimonials from "@/components/UI/testimonials";
 
@@ -29,17 +26,17 @@ const HomePage = () => {
   return (
     <div>
       <div className="bg-[url('/images/search-background.jpg')] bg-cover bg-center">
-        <div className="bg-gradient-to-b from-[#82C341E5] to-[#2BA149E5] p-4 shadow-md">
+        <div className="from-light-primary-transparent to-primary-transparent bg-gradient-to-b p-4 shadow-md">
           <Header />
           <div className="container mx-auto flex flex-col-reverse items-center gap-6 p-4 md:flex-row lg:max-w-[1170px]">
             <div className="col-span-4 md:col-span-3">
-              <h2 className="mb-6 text-[45px] font-black leading-none text-white md:text-[60px]">
-                <span className="text-[45px] font-black text-[#101828] md:text-[60px]">
+              <h2 className="text-primary-foreground mb-6 text-[45px] font-black leading-none md:text-[60px]">
+                <span className="text-main text-[45px] font-black md:text-[60px]">
                   Discover
                 </span>{" "}
                 More <br />
                 Than 5000
-                <span className="text-[45px] font-black text-[#101828] md:text-[60px]">
+                <span className="text-main text-[45px] font-black md:text-[60px]">
                   {" "}
                   + Jobs
                 </span>{" "}
@@ -49,32 +46,34 @@ const HomePage = () => {
               </Suspense>
               <p className="mt-4 text-gray-100">
                 {" "}
-                <span className="font-bold text-white">Popular</span> : Medical
-                Claims Officer,Dental Designer, Healthcare- Presales specialist,
-                Medical Ambassador
+                <span className="text-primary-foreground font-bold">
+                  Popular
+                </span>{" "}
+                : Medical Claims Officer,Dental Designer, Healthcare- Presales
+                specialist, Medical Ambassador
               </p>
               <div className="mt-4 flex flex-wrap gap-4 text-nowrap">
                 <Link
                   href="/search?q=Pediatric Consultant"
-                  className="rounded-full border border-white px-4 py-2 text-sm text-white transition-colors duration-300 hover:bg-white hover:text-[#2BA149] focus:ring-2 focus:ring-white"
+                  className="text-primary-foreground hover:text-primary rounded-full border border-white px-4 py-2 text-sm transition-colors duration-300 hover:bg-white focus:ring-2 focus:ring-white"
                 >
                   Pediatric Consultant
                 </Link>
                 <Link
                   href="/search?q=ICU Nurse"
-                  className="rounded-full border border-white px-4 py-2 text-sm text-white transition-colors duration-300 hover:bg-white hover:text-[#2BA149] focus:ring-2 focus:ring-white"
+                  className="text-primary-foreground hover:text-primary rounded-full border border-white px-4 py-2 text-sm transition-colors duration-300 hover:bg-white focus:ring-2 focus:ring-white"
                 >
                   ICU Nurse
                 </Link>
                 <Link
                   href="/search?q=Obsteric Consultant Saudi Arabia"
-                  className="rounded-full border border-white px-4 py-2 text-sm text-white transition-colors duration-300 hover:bg-white hover:text-[#2BA149] focus:ring-2 focus:ring-white"
+                  className="text-primary-foreground hover:text-primary rounded-full border border-white px-4 py-2 text-sm transition-colors duration-300 hover:bg-white focus:ring-2 focus:ring-white"
                 >
                   Obsteric Consultant Saudi Arabia
                 </Link>
                 <Link
                   href="/search?q=Internal Medicine Registrar Oman"
-                  className="rounded-full border border-white px-4 py-2 text-sm text-white transition-colors duration-300 hover:bg-white hover:text-[#2BA149] focus:ring-2 focus:ring-white"
+                  className="text-primary-foreground hover:text-primary rounded-full border border-white px-4 py-2 text-sm transition-colors duration-300 hover:bg-white focus:ring-2 focus:ring-white"
                 >
                   Internal Medicine Registrar Oman
                 </Link>
@@ -96,17 +95,17 @@ const HomePage = () => {
           <div className="bg-neutral-100/60">
             <div className="container mx-auto min-h-screen p-2 pt-16 lg:max-w-[1170px]">
               <div className="flex justify-center p-4">
-                <div className="rounded-full bg-[#2BA149] px-6 py-2 text-white">
+                <div className="text-primary-foreground bg-primary rounded-full px-6 py-2">
                   Job Categories
                 </div>
               </div>
-              <h2 className="mb-6 text-center text-[45px] font-bold leading-none text-[#82C341] md:text-[60px]">
-                <span className="text-[45px] font-bold text-[#101828] md:text-[60px]">
+              <h2 className="text-light-primary mb-6 text-center text-[45px] font-bold leading-none md:text-[60px]">
+                <span className="text-main text-[45px] font-bold md:text-[60px]">
                   Explore Jobs
                 </span>{" "}
                 By Specialist
               </h2>
-              <p className="mx-auto mb-16 max-w-[700px] text-center text-2xl text-gray-500">
+              <p className="text-secondary mx-auto mb-16 max-w-[700px] text-center text-2xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry&apos;s standard
                 dummy
@@ -126,7 +125,7 @@ const HomePage = () => {
                     />
                   </div>
                 </div>
-                <div className="z-0 col-start-1 row-start-1 h-full w-full translate-y-2 rounded-full bg-green-600"></div>
+                <div className="bg-primary z-0 col-start-1 row-start-1 h-full w-full translate-y-2 rounded-full"></div>
               </div>
             </div>
           </div>
@@ -134,13 +133,13 @@ const HomePage = () => {
 
         {/* popular country  */}
         <div className="container mx-auto min-h-screen p-2 pt-16 lg:max-w-[1170px]">
-          <h2 className="my-6 text-center text-[45px] font-bold leading-none text-[#82C341] md:text-[60px]">
-            <span className="text-[45px] font-bold text-[#101828] md:text-[60px]">
+          <h2 className="text-light-primary my-6 text-center text-[45px] font-bold leading-none md:text-[60px]">
+            <span className="text-main text-[45px] font-bold md:text-[60px]">
               Popular
             </span>{" "}
             Country
           </h2>
-          <p className="mx-auto mb-16 max-w-[700px] text-center text-2xl text-gray-500">
+          <p className="text-secondary mx-auto mb-16 max-w-[700px] text-center text-2xl">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
           </p>
@@ -148,7 +147,7 @@ const HomePage = () => {
             {countries.map((country, i) => (
               <div
                 key={i}
-                className="my-4 flex items-center gap-4 rounded-[10px] border border-gray-200 bg-white shadow-xl duration-300 hover:scale-105 hover:border-green-600 hover:shadow-2xl"
+                className="hover:border-light-primary my-4 flex items-center gap-4 rounded-[10px] border border-gray-200 bg-white shadow-xl duration-300 hover:scale-105 hover:shadow-2xl"
               >
                 <Flag
                   code={country.code.toLocaleLowerCase()}
@@ -165,13 +164,13 @@ const HomePage = () => {
         <div className="bg-opacity-10 bg-[url('/images/hiring-bg.jpg')] bg-cover bg-center">
           <div className="bg-neutral-100/60">
             <div className="container mx-auto min-h-screen p-2 py-16 lg:max-w-[1170px]">
-              <h2 className="my-6 text-center text-[45px] font-bold leading-none text-[#82C341] md:text-[60px]">
-                <span className="text-[45px] font-bold text-[#101828] md:text-[60px]">
+              <h2 className="text-light-primary my-6 text-center text-[45px] font-bold leading-none md:text-[60px]">
+                <span className="text-main text-[45px] font-bold md:text-[60px]">
                   Discover who is hiring on
                 </span>{" "}
                 Medicova
               </h2>
-              <p className="mx-auto mb-8 max-w-[700px] text-center text-2xl text-gray-500">
+              <p className="text-secondary mx-auto mb-8 max-w-[700px] text-center text-2xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry&apos;s standard
                 dummy
@@ -191,31 +190,29 @@ const HomePage = () => {
         </div>
 
         {/* 50% Discount  */}
-        <div className="bg-gradient-to-tr from-[#2BA149] to-[#82C341]">
+        <div className="from-primary to-light-primary bg-gradient-to-tr">
           <div className="bg-opacity-10 bg-[url('/images/honey.svg')] bg-contain bg-center">
             <div className="container mx-auto px-2 pt-16 lg:max-w-[1170px]">
               <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-3 md:grid-rows-1">
                 <div className="md:my-6">
-                  <h2 className="mb-4 text-[45px] font-bold leading-none text-white md:text-[60px]">
+                  <h2 className="text-primary-foreground mb-4 text-[45px] font-bold leading-none md:text-[60px]">
                     Discover more
-                    <span className="text-[45px] font-bold text-[#101828] md:text-[60px]">
+                    <span className="text-main text-[45px] font-bold md:text-[60px]">
                       {" "}
                       is <br /> our{" "}
                     </span>{" "}
                     Priority
                   </h2>
-                  <p className="max-w-80 text-4xl font-bold text-white">
+                  <p className="text-primary-foreground max-w-80 text-4xl font-bold">
                     “We provide the perfect healthcare service”
                   </p>
                 </div>
                 <div className="z-[1] flex items-end justify-center py-6">
                   <div className="flex h-[200px] w-[200px] flex-col items-center justify-center rounded-full bg-white text-center shadow-2xl">
-                    <h6 className="text-[60px] font-black text-green-500">
+                    <h6 className="text-light-primary text-[60px] font-black">
                       50%
                     </h6>
-                    <p className="text-[24px] font-black text-[#101828]">
-                      Discount
-                    </p>
+                    <p className="text-main text-[24px] font-black">Discount</p>
                   </div>
                 </div>
                 <div className="relative z-0 hidden md:block">
@@ -243,13 +240,13 @@ const HomePage = () => {
         <div className="bg-[url('/images/jobs-background.jpg')] bg-cover bg-center">
           <div className="bg-white/80 shadow-md">
             <div className="container mx-auto p-4 lg:max-w-[1170px]">
-              <h2 className="my-6 text-center text-[45px] font-bold leading-none text-[#82C341] md:text-[60px]">
-                <span className="text-[45px] font-bold text-[#101828] md:text-[60px]">
+              <h2 className="text-light-primary my-6 text-center text-[45px] font-bold leading-none md:text-[60px]">
+                <span className="text-main text-[45px] font-bold md:text-[60px]">
                   Latest jobs
                 </span>{" "}
                 open
               </h2>
-              <p className="mx-auto mb-8 max-w-[700px] text-center text-2xl text-gray-500">
+              <p className="text-secondary mx-auto mb-8 max-w-[700px] text-center text-2xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry&apos;s standard
                 dummy
@@ -260,7 +257,7 @@ const HomePage = () => {
                 {[1, 2, 3, 1, 2, 3, 1, 2, 3].map((_, i) => (
                   <button
                     key={i}
-                    className="flex gap-4 rounded-[10px] border border-gray-100 bg-white p-4 shadow-xl transition-all duration-300 hover:scale-105 hover:border-[#2BA149] hover:shadow-2xl focus:ring-2 focus:ring-[#2BA149]"
+                    className="hover:border-primary focus:ring-primary flex gap-4 rounded-[10px] border border-gray-100 bg-white p-4 shadow-xl transition-all duration-300 hover:scale-105 hover:shadow-2xl focus:ring-2"
                   >
                     <Image
                       src="/images/company-logo.jpg"
@@ -270,20 +267,20 @@ const HomePage = () => {
                       className="h-[90px] rounded-md object-contain"
                     />
                     <div>
-                      <h6 className="my-1 text-left font-semibold">
+                      <h6 className="text-main my-1 text-left font-semibold">
                         Physical therapist
                       </h6>
-                      <p className="text-left text-sm font-medium text-gray-500">
+                      <p className="text-secondary text-left text-sm font-medium">
                         Nomad Paris, France
                       </p>
                       <div className="mt-1 flex flex-wrap gap-2">
-                        <span className="rounded-md border border-gray-200 bg-gray-50 p-1 px-2 text-xs">
+                        <span className="text-main rounded-md border border-gray-200 bg-gray-50 p-1 px-2 text-xs">
                           Full-Time
                         </span>
-                        <span className="rounded-md border border-green-300 p-1 px-2 text-xs text-green-400">
+                        <span className="border-light-primary text-light-primary rounded-md border p-1 px-2 text-xs">
                           Full-Time
                         </span>
-                        <span className="rounded-md border border-green-300 p-1 px-2 text-xs text-green-400">
+                        <span className="border-light-primary text-light-primary rounded-md border p-1 px-2 text-xs">
                           Full-Time
                         </span>
                       </div>
@@ -294,7 +291,7 @@ const HomePage = () => {
               <div className="mt-8 flex justify-center">
                 <Link
                   href="#"
-                  className="rounded-[8px] bg-[#2BA149] px-6 py-3 font-semibold uppercase text-white transition-colors duration-300 hover:bg-white hover:text-[#2BA149] focus:ring-2 focus:ring-white"
+                  className="text-primary-foreground bg-primary hover:text-primary hover:bg-primary-foreground rounded-[8px] px-6 py-3 font-semibold uppercase transition-colors duration-300 focus:ring-2 focus:ring-white"
                 >
                   Explore All
                 </Link>
@@ -304,15 +301,15 @@ const HomePage = () => {
         </div>
 
         {/* Jobseeker Testimonials */}
-        <div className="bg-gradient-to-tr from-[#2BA149] to-[#82C341]">
+        <div className="from-primary to-light-primary bg-gradient-to-tr">
           <div className="container mx-auto p-2 py-6 lg:max-w-[1170px]">
-            <h2 className="my-6 text-center text-[45px] font-bold leading-none text-[#101828] md:text-[60px]">
+            <h2 className="text-main my-6 text-center text-[45px] font-bold leading-none md:text-[60px]">
               Jobseeker{" "}
-              <span className="text-[45px] font-bold text-white md:text-[60px]">
+              <span className="text-primary-foreground text-[45px] font-bold md:text-[60px]">
                 Testimonials
               </span>
             </h2>
-            <p className="mx-auto mb-8 max-w-[700px] text-center text-2xl text-white">
+            <p className="text-primary-foreground mx-auto mb-8 max-w-[700px] text-center text-2xl">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry&apos;s standard dummy
             </p>
@@ -320,7 +317,7 @@ const HomePage = () => {
             <div className="mt-8 flex justify-center">
               <Link
                 href="#"
-                className="rounded-[8px] bg-white px-6 py-3 font-semibold uppercase text-[#2BA149] transition-colors duration-300 hover:bg-transparent hover:text-white focus:ring-2 focus:ring-white"
+                className="hover:text-primary-foreground text-primary bg-primary-foreground rounded-[8px] px-6 py-3 font-semibold uppercase transition-colors duration-300 hover:bg-transparent focus:ring-2 focus:ring-white"
               >
                 Explore All
               </Link>
@@ -331,14 +328,14 @@ const HomePage = () => {
         <div className="bg-[url('/images/jobs-background.jpg')] bg-cover bg-center">
           <div className="bg-white/80 shadow-md">
             <div className="container mx-auto min-h-screen p-4 lg:max-w-[1170px]">
-              <h2 className="my-6 text-center text-[45px] font-bold leading-none text-[#101828] md:text-[60px]">
+              <h2 className="text-main my-6 text-center text-[45px] font-bold leading-none md:text-[60px]">
                 Find Job Vacancies
-                <span className="text-[45px] font-bold text-[#82C341] md:text-[60px]">
+                <span className="text-light-primary text-[45px] font-bold md:text-[60px]">
                   {" "}
                   BY
                 </span>
               </h2>
-              <p className="mx-auto mb-8 max-w-[700px] text-center text-2xl text-gray-500">
+              <p className="text-secondary mx-auto mb-8 max-w-[700px] text-center text-2xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry&apos;s standard
                 dummy
