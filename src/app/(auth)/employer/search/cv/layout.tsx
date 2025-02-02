25c83e9b9ecf389bbc49828db4cec3c853ea40f5
AbdElRahman501 <abdelrahman.6182@gmail.com>
2025-02-02 09:20:59 +0200

    fix error

--- src/app/(auth)/employer/search/cv/layout.tsx
+++ src/app/(auth)/employer/search/cv/layout.tsx
@@ -1,5 +1,6 @@
 import { searchFilters } from "@/constants";
 import FilterSideBar from "@/components/Layout/filter/silter-sidebar";
+import { Suspense } from "react";
 
 const layout: React.FC<{
   children: React.ReactNode;
@@ -7,10 +8,18 @@ const layout: React.FC<{
   return (
     <div className="container mx-auto my-8 flex min-h-screen w-full flex-row gap-5 p-2 lg:max-w-[1170px]">
       {/* Left Column: Filter Section */}
-      <FilterSideBar
-        sections={searchFilters}
-        searchKeys={["Residency (Location)", "nationality"]}
-      />
+      <Suspense
+        fallback={
+          <div className="hidden w-1/5 rounded-[10px] border border-gray-100 bg-white p-[20px] shadow-xl lg:block">
+            Loading...
+          </div>
+        }
+      >
+        <FilterSideBar
+          sections={searchFilters}
+          searchKeys={["Residency (Location)", "nationality"]}
+        />
+      </Suspense>
 
       {children}
     </div>
