068e34bbe6e08ee4e3bf1d1953b3f6742a09ee5d
AbdElRahman501 <abdelrahman.6182@gmail.com>
2025-04-10 22:11:27 +0200

    feat: Add StatusCard and EmployerList components for admin dashboard

---
+++ src/app/(auth)/admin/components/employers/EmployerList.tsx
@@ -0,0 +1,16 @@
+"use client";
+const EmployerList: React.FC = () => {
+  const API_GET_EMPLOYERS_TABLE_DATA = "/api/employers/table-data";
+  return (
+    <>
+      <div className="box-content !p-0">
+        {/* <OverviewEmployersTable
+          endPoint={API_GET_EMPLOYERS_TABLE_DATA}
+          Filtring={true}
+        /> */}
+      </div>
+    </>
+  );
+};
+
+export default EmployerList;
